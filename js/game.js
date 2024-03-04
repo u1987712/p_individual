0c223c4cc209b440e59a5df4e04431f666ef400b
u1987712 <u1987712@campus.udg.edu>
2024-03-04 17:12:10 +0100

    Updated files

--- js/game.js
+++ js/game.js
@@ -1,5 +1,15 @@
 import { game as gController } from "./memory.js";
 
+//Afegim elements
+var game = $('#game');
+game.append('<img id="cb" title="card" >');
+game.append('<img id="co" title="card" >');
+game.append('<img id="sb" title="card" >');
+game.append('<img id="so" title="card" >');
+game.append('<img id="tb" title="card" >');
+game.append('<img id="to" title="card" >');
+
+//Obtenim valors
 var cb = $('#cb');
 var co = $('#co');
 var sb = $('#sb');
@@ -7,6 +17,8 @@ var so = $('#so');
 var tb = $('#tb');
 var to = $('#to');
 
+
+//Detectem canvis
 cb.on('click', function () { gController("cb") });
 co.on('click', function () { gController("co") });
 sb.on('click', function () { gController("sb") });
@@ -14,6 +26,8 @@ so.on('click', function () { gController("so") });
 tb.on('click', function () { gController("tb") });
 to.on('click', function () { gController("to") });
 
+
+//Modifiquem valors
 cb.attr("src","../resources/cb.png");
 co.attr("src","../resources/co.png");
 sb.attr("src","../resources/sb.png");
@@ -21,7 +35,6 @@ so.attr("src","../resources/so.png");
 tb.attr("src","../resources/tb.png");
 to.attr("src","../resources/to.png");
 
-var game = $('#game');
 
 gController.init(updateSRC).forEach(function(card, indx){
     game.append('<img id="c'+indx+'" class="card" title="card">');
