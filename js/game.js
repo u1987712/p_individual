479a7e3d4dfba980decc5653d6143a264b31f13d
u1987712 <u1987712@campus.udg.edu>
2024-03-04 17:03:42 +0100

    updated file

--- js/game.js
+++ js/game.js
@@ -1,5 +1,26 @@
 import { game as gController } from "./memory.js";
 
+var cb = $('#cb');
+var co = $('#co');
+var sb = $('#sb');
+var so = $('#so');
+var tb = $('#tb');
+var to = $('#to');
+
+cb.on('click', function () { gController("cb") });
+co.on('click', function () { gController("co") });
+sb.on('click', function () { gController("sb") });
+so.on('click', function () { gController("so") });
+tb.on('click', function () { gController("tb") });
+to.on('click', function () { gController("to") });
+
+cb.attr("src","../resources/cb.png");
+co.attr("src","../resources/co.png");
+sb.attr("src","../resources/sb.png");
+so.attr("src","../resources/so.png");
+tb.attr("src","../resources/tb.png");
+to.attr("src","../resources/to.png");
+
 var game = $('#game');
 
 gController.init(updateSRC).forEach(function(card, indx){
