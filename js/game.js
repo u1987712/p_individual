0ef2232f11180e7129b4d6341e5bb1ff281a6ff4
u1987712 <u1987712@campus.udg.edu>
2024-03-04 17:53:01 +0100

    updated files

--- js/game.js
+++ js/game.js
@@ -2,38 +2,46 @@ import { game as gController } from "./memory.js";
 
 //Afegim elements
 var game = $('#game');
-game.append('<img id="cb" title="card" >');
-game.append('<img id="co" title="card" >');
-game.append('<img id="sb" title="card" >');
-game.append('<img id="so" title="card" >');
-game.append('<img id="tb" title="card" >');
-game.append('<img id="to" title="card" >');
+
+items.forEach(function(value, indx){
+    game.append('<img id="c'+indx+'"title="card">');
+    var c = $ ('#c'+indx);
+    c.on('click', function() { gController(value) });
+    c.attr("src", value);
+})
+
+game.append('<img id="c1" title="card" >');
+game.append('<img id="c2" title="card" >');
+game.append('<img id="c3" title="card" >');
+game.append('<img id="c4" title="card" >');
+game.append('<img id="c5" title="card" >');
+game.append('<img id="c6" title="card" >');
 
 //Obtenim valors
-var cb = $('#cb');
-var co = $('#co');
-var sb = $('#sb');
-var so = $('#so');
-var tb = $('#tb');
-var to = $('#to');
+var c1 = $('#c1');
+var c2 = $('#c2');
+var c3 = $('#c3');
+var c4 = $('#c4');
+var c5 = $('#c5');
+var c6 = $('#c6');
 
 
 //Detectem canvis
-cb.on('click', function () { gController("cb") });
-co.on('click', function () { gController("co") });
-sb.on('click', function () { gController("sb") });
-so.on('click', function () { gController("so") });
-tb.on('click', function () { gController("tb") });
-to.on('click', function () { gController("to") });
+c1.on('click', function () { gController("cb") });
+c2.on('click', function () { gController("co") });
+c3.on('click', function () { gController("sb") });
+c4.on('click', function () { gController("so") });
+c5.on('click', function () { gController("tb") });
+c6.on('click', function () { gController("to") });
 
 
 //Modifiquem valors
-cb.attr("src","../resources/cb.png");
-co.attr("src","../resources/co.png");
-sb.attr("src","../resources/sb.png");
-so.attr("src","../resources/so.png");
-tb.attr("src","../resources/tb.png");
-to.attr("src","../resources/to.png");
+c1.attr("src","../resources/cb.png");
+c2.attr("src","../resources/co.png");
+c3.attr("src","../resources/sb.png");
+c4.attr("src","../resources/so.png");
+c5.attr("src","../resources/tb.png");
+c6.attr("src","../resources/to.png");
 
 
 gController.init(updateSRC).forEach(function(card, indx){
