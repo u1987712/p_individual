e85efa7aac67a8a42bd0e8e3b81841bb4f5e0bc0
GemReina <147042721+GemReina@users.noreply.github.com>
2024-06-26 10:00:17 +0200

    T.T
    me deje un jquery ups

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,23 @@
 $(document).ready(function() {
+    console.log('Document is ready');
+
     $('#play').click(function() {
+        console.log('Play button clicked');
         window.location.assign("./html/game.html");
     });
 
     $('#options').click(function() {
+        console.log('Options button clicked');
         window.location.assign("./html/options.html");
     });
 
     $('#saves').click(function() {
+        console.log('Saves button clicked');
         console.error("Opció no implementada");
     });
 
     $('#exit').click(function() {
+        console.log('Exit button clicked');
         console.warn("No es pot sortir!");
     });
 });
